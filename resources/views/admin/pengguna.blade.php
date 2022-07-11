90d0031d096fd005f8d561dda8d551e26434aad4
Fayi Awaluddin Zaki <zakiawaluddin@gmail.com>
2022-07-11 17:24:31 +0700

    update

--- resources/views/admin/pengguna.blade.php
+++ resources/views/admin/pengguna.blade.php
@@ -9,7 +9,7 @@
 
 {{ $data }}
 
-<form action="POST">
+{{-- <form action="POST">
     <div class="form-group">
       <label for="exampleInputEmail1">Nama</label>
       <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
@@ -23,15 +23,35 @@
       <label for="exampleInputPassword1">Password</label>
       <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
     </div>
-    {{-- <div class="form-group">
-      <label for="exampleInputPassword1">Password</label>
-      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-    </div> --}}
     <div class="form-check">
       <input type="checkbox" class="form-check-input" id="exampleCheck1">
       <label class="form-check-label" for="exampleCheck1">Check me out</label>
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
+</form> --}}
+
+<form action="" method="POST" enctype="multipart/form-data">
+                        
+    @csrf
+
+    <div class="form-group">
+        <label class="font-weight-bold">Nama</label>
+        <input type="text" class="form-control @error is-invalid @enderror" name="name">
+    </div>
+
+    <div class="form-group">
+        <label class="font-weight-bold">Email</label>
+        <input type="text" class="form-control @error('email') is-invalid @enderror" name="email">
+    </div>
+
+    <div class="form-group">
+        <label class="font-weight-bold">Password</label>
+        <input type="password" class="form-control @error('email') is-invalid @enderror" name="password">
+    </div>
+
+    <button type="submit" class="btn btn-md btn-primary">SIMPAN</button>
+    <button type="reset" class="btn btn-md btn-warning">RESET</button>
+
+</form> 
 
 @endsection
