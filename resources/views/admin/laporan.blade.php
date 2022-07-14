ec944f011c5060ac17cd5ed068b98fb09dc60838
SabrinaHeryantiSuryandari <19081010130@student.upnjatim.ac.id>
2022-07-14 23:03:24 +0700

    first commit

--- resources/views/admin/laporan.blade.php
+++ resources/views/admin/laporan.blade.php
@@ -35,7 +35,7 @@
                 <table class="table table-bordered" id="example" style="width:100%">
                     <thead>
                     <tr>
-                        <th></th>
+                        <th>NO</th>
                         <th>No Sertifikat</th>
                         <th>Nama Aset</th>
                         <th>Alamat</th>
@@ -54,7 +54,7 @@
                     <tbody>
                     @foreach ($asets as $aset)
                     <tr>
-                        <td>{{ $aset->id }}</td>
+                        <td>{{ ++$i }}</td>
                         <td>{{ $aset->no_sertifikat }}</td>
                         <td>{{ $aset->nama_aset }}</td>
                         <td>{{ $aset->alamat_aset }}</td>
@@ -70,12 +70,12 @@
                         <td>
                             <form action="{{ route('asets.destroy',$aset->id) }}" method="POST">
             
-                                <a class="btn btn-info" href="{{ route('asets.show',$aset->id) }}">Detail</a>
+                                <a data-toggle="modal" data-target="#exampleModal" class="btn btn-info" href="{{ route('asets.show',$aset->id) }}">Detail</a>
                 
-{{-- <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-    Launch demo modal
-  </button>
+<!-- Button trigger modal -->
+{{-- <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+    Detail
+</button> --}}
   
   <!-- Modal -->
   <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -88,7 +88,7 @@
           </button>
         </div>
         <div class="modal-body">
-            @foreach ($asets as $aset)
+            {{-- @foreach ($asets as $aset) --}}
             <table class="" style="margin:20px auto;" id="dataTable" width="100%" cellspacing="0">
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
@@ -169,7 +169,7 @@
                             <td>{{ $aset->th_pembangunan }}</td>
                         </tr>
                     </div>
-                </div> --}}
+                </div> 
                 {{-- <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
                         <tr>
@@ -178,7 +178,7 @@
                         </tr>
                     </div>
                 </div> --}}
-                {{-- <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
                         <tr>
                             <td>Tahun Rehab</td>
@@ -195,7 +195,7 @@
                     </div>
                 </div>
             </table>
-            @endforeach
+            {{-- @endforeach --}}
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -203,7 +203,7 @@
         </div>
       </div>
     </div>
-  </div> --}}
+  </div>
 
                                 <a class="btn btn-primary" href="{{ route('asets.edit',$aset->id) }}">Edit</a>
             
